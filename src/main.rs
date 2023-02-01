2d6e66f4c91683a01aa78b329a14e905d7d3c5a9
Anshal Jain <pk.ness.ness@gmail.com>
2023-01-31 17:51:59 -0800

    starting to make a window
    also made compile times faster? at the cost of first compile being long as shit

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 use bevy::prelude::*;
 
+pub const HEIGHT: f32 = 720.0;
+pub const WIDTH: f32 = 1280.0;
+
 fn main() {
-    println!("Hello, world!");
-    App::new().run();
+    App::new()
+    .insert_resource(ClearColor(Color::rgb(0.3,0.2,0.4)))
+    .add_startup_system(spawn_camera)
+    .add_plugins(DefaultPlugins)
+    .run();
+}
+
+fn spawn_camera(mut commands: Commands){
+    commands.spawn(Camera3dBundle {
+        transform: Transform::from_xyz(0.2,0.1,0.3).looking_at(Vec3::ZERO, Vec3::Y),
+        ..default()
+    });
 }

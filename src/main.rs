98a6175b1bdfdf8cb00941a4b7df81b9137be2a3
Anshal Jain <pk.ness.ness@gmail.com>
2023-01-31 23:49:57 -0800

    added simple cube and plane, began to create a model for the hexes

--- src/main.rs
+++ src/main.rs
@@ -7,13 +7,33 @@ fn main() {
     App::new()
     .insert_resource(ClearColor(Color::rgb(0.3,0.2,0.4)))
     .add_startup_system(spawn_camera)
+    .add_startup_system(spawn_scene)
     .add_plugins(DefaultPlugins)
     .run();
 }
 
 fn spawn_camera(mut commands: Commands){
     commands.spawn(Camera3dBundle {
-        transform: Transform::from_xyz(0.2,0.1,0.3).looking_at(Vec3::ZERO, Vec3::Y),
+        transform: Transform::from_xyz(2.2,1.1,4.3).looking_at(Vec3::ZERO, Vec3::Y),
         ..default()
     });
+}
+
+fn spawn_scene(
+    mut commands: Commands, 
+    mut meshes: ResMut<Assets<Mesh>>,
+    mut materials: ResMut<Assets<StandardMaterial>>,
+) {
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Plane {size: 5.0})),
+        material: materials.add(Color::rgb(0.6,0.2,0.8).into()),
+        ..default()
+    });
+    commands.spawn(PbrBundle {
+        mesh: meshes.add(Mesh::from(shape::Cube {size: 1.0})),
+        material: materials.add(Color::rgb(0.6,0.2,0.8).into()),
+        transform: Transform::from_xyz(0.0,0.5,0.0),
+        ..default()
+    });
+    
 }
